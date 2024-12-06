379a5ad27969dadbcbcbf6b70b6dbccd2d02e084
Shubhhhwarrior <shubhhhwarrior@gmail.com>
2024-12-07 00:10:22 +0530

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
 /**
  * @copyright (c) 2024 - Present
+ * @author github.com/KunalG932
  * @license MIT
  */
 
 import { SessionProvider } from 'next-auth/react';
 import { ToastContainer } from 'react-toastify';
-import { Analytics } from '@vercel/analytics/react'; // Import Vercel Analytics
 import ErrorBoundary from '@/components/ErrorBoundary';
 import type { AppProps } from 'next/app';
 import 'react-toastify/dist/ReactToastify.css';
@@ -16,7 +16,6 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
     <ErrorBoundary>
       <SessionProvider session={session}>
         <Component {...pageProps} />
-        <Analytics /> {/* Add Vercel Analytics */}
         <ToastContainer
           position="top-right"
           autoClose={5000}
@@ -29,13 +28,7 @@ export default function App({ Component, pageProps: { session, ...pageProps } }:
           pauseOnHover
           theme="light"
         />
-        {/* Add Policies Page Link */}
-        <footer style={{ textAlign: 'center', marginTop: '20px' }}>
-          <a href="/policies" style={{ textDecoration: 'none', color: '#0070f3' }}>
-            Policies
-          </a>
-        </footer>
       </SessionProvider>
     </ErrorBoundary>
   );
-}
+} 
